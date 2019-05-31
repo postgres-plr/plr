a958ceb7244535ec351eed2aff04725018bb5a5b
Dave Cramer <davecramer@gmail.com>
2019-05-31 17:15:53 -0400

    add dir-locals and editorconfig to allow github to format patches properly (#60)

---
+++ .dir-locals.el
@@ -0,0 +1,18 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
+  (bug-reference-url-format . "https://github.com/postgres-plr/plr/issues/%s"))
+ (c-mode
+  (c-basic-offset . 4)
+  (c-file-style . "bsd")
+  (fill-column . 78)
+  (indent-tabs-mode . t)
+  (tab-width . 4))
+ (ess-r-mode
+  (ess-style . RStudio))
+ (sql-mode
+  (sql-indent-offset . 2)
+  (fill-column . 78)
+  (indent-tabs-mode)))
