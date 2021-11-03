cca7dc82feb8e5a898dcc8619fc581c37d8c9cf6
Dave Cramer <davecramer@gmail.com>
2021-11-03 11:33:47 -0400

    bump version to 8.4.5, added upgrade sql files

---
+++ plr--8.4.4--8.4.5.sql
@@ -0,0 +1,4 @@
+-- no changes to the sql files. 
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION plr UPDATE to '8.4.5'" to load this file. \quit
--- plr-8.4.2--8.4.3.sql
+++
@@ -1,1 +0,0 @@
--- no changes to the sql files. Changes were require due to postgres API deprecation
