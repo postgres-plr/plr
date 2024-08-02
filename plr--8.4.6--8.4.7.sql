640483368c1ef053bb8bc9214eabaf6ac51b447c
Dave Cramer <davecramer@gmail.com>
2024-08-02 08:43:39 -0400

    prepare for release version 8_4_7 (#157)
    * prepare for release version 8_4_7
    
    * bump version
    
    * update versions to STABLE

---
+++ plr--8.4.6--8.4.7.sql
@@ -0,0 +1,4 @@
+-- no changes to the sql files.
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION plr UPDATE to '8.4.7'" to load this file. \quit
